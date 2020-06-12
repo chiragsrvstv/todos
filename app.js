1bd710e84c12f7fbd2cd6c20bc79d35b928b881c
chiragsrvstv <chirag.srivastava@live.com>
2020-06-12 20:17:00 +0530

    create todo

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+
+//Parse URL-encoded bodies
+app.use(express.urlencoded()); 
 
 // server will listen for localhost
 app.listen(3000, function () {
   console.log("serving todos app on port 3000");
 });
 
-app.get('/', (req, res) => {
-    res.redirect('/todos')
-})
+app.get("/", (req, res) => {
+  res.redirect("/todos");
+});
 
 app.get("/todos", (req, res) => {
-    res.render("todos.ejs")
-})
+  res.render("todos.ejs");
+});
 
+app.get("/todos/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// creating a new todo
+app.post("/todos", (req, res) => {
+  const todoName = req.body.todoName;
+    console.log(todoName);
+  // create a new todo in db here
+
+  res.redirect("/todos");
+});
