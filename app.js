e6a85ca3a4202c409ed6251f6d9916f512ef56e6
chiragsrvstv <chirag.srivastava@live.com>
2020-06-13 01:13:47 +0530

    edit

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 
 const db = require("./db/config.js");
 
+// method override for PUT
+const methodOverride = require("method-override");
+
 app.use(express.static("public"));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 //Parse URL-encoded bodies
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // server will listen for localhost
 app.listen(3000, function () {
@@ -45,13 +49,45 @@ app.post("/todos", (req, res) => {
     values: [todoName, todoCategory],
   })
     .then(() => {
-      console.log("Created")
+      console.log("Created");
+      res.redirect("/todos");
     })
     .catch((error) => {
       res.render("error.ejs", { errorMessage: error.message });
     });
-  res.redirect("/todos");
 });
+
+// edit route
+app.get("/todos/:id/edit", (req, res) => {
+  const id = req.params.id;
+  db.one({
+    text: "select * from todolist where id=$1",
+    values: [id],
+  })
+    .then((data) => {
+      res.render("edit.ejs", { data: data });
+    })
+    .catch((error) => {
+      res.render("error.ejs", { errorMessage: error.message });
+    });
+});
+
+app.put("/todos/:id", (req, res) => {
+  const todoName = req.body.todoName;
+  const todoCategory = req.body.todoCategory;
+  const id = req.params.id;
+  db.none({
+    text: "update todolist set title=$1, category=$2 where id=$3",
+    values: [todoName, todoCategory, id],
+  })
+    .then(() => {
+      res.redirect("/todos");
+    })
+    .catch((error) => {
+      res.render("error.ejs", { errorMessage: error.message });
+    });
+});
+
 
 // handling bad routes
 app.get("*", (req, res) => {
