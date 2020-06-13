78b75c9c6312dbd392cad4de656c96e9d7199faa
chiragsrvstv <chirag.srivastava@live.com>
2020-06-13 18:18:46 +0530

    common database name

--- db/config.js
+++ db/config.js
@@ -7,7 +7,7 @@ const pgp = require('pg-promise')();
 const connection = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE_NAME,
+    database: 'todolist',
     port: process.env.DB_DBPORT,
     
 }
